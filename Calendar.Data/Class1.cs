47000b87ebe2bd095eb9895349ee23c982d9af00
Paweł Sroczyński <p.sroczynski@aeroshop.pl>
2015-03-16 13:08:15 +0100

    Project structure refactor.

---
+++ Calendar.Data/Class1.cs
@@ -0,0 +1,23 @@
+﻿using Simple.Data;
+
+namespace Calendar.Data
+{
+    public static class DbHelper
+    {
+        public static InMemoryAdapter CreateStructures()
+        {
+            var adapter = new InMemoryAdapter();
+            adapter.SetAutoIncrementKeyColumn("Events", "Id");
+            return adapter;
+        }
+
+        //public static void InitDb()
+        //{
+        //    var adapter = CreateStructures();
+        //    Database.UseMockAdapter(adapter);
+
+        //    var db = Database.Open();
+        //    db.Test.Insert(Id: 1, Name: "Alice");
+        //}
+    }
+}
